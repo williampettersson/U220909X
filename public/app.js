31e61cb0d5e4bb06ea77a1f396ed17c91414083b
williampettersson <william.c.pettersson@gmail.com>
2022-09-12 13:04:20 +0200

    Added a new container div with two text elements and some buttons

--- public/app.js
+++ public/app.js
@@ -20,23 +20,53 @@ function build() {
     home.className = 'btn btn-light ms-3 fa fa-home';
     navbar.appendChild(home);
 
+    const github = document.createElement('button');  // Här skapar jag en githubknapp i navigationbar.
+    github.className = 'btn btn-light ms-3 me-3 fa fa-github';
+    navbar.appendChild(github);
+
+    const mail = document.createElement('button');  // Här skapar jag en mailnapp i navigationbar.
+    mail.className = 'btn btn-light ms-3 me-3 fa fa-envelope';
+    navbar.appendChild(mail);
+
     const titleLeft = document.createElement('h1'); //Skapar en titel för den vänstra diven. 
     titleLeft.innerHTML = 'William Pettersson';
     titleLeft.className = 'h1 mt-3 ms-5';
     left.appendChild(titleLeft);
 
     const aboutDiv = document.createElement('div'); // Här skapar jag en div för textelementet.
-    aboutDiv.className = 'w-75 vh- mt-1 mg-3 ms-5 bg-dark text-light border';
+    aboutDiv.className = 'w-75 h-25 mt-2 mg-3 ms-5 text-light';
     left.appendChild(aboutDiv);
 
     const aboutText = document.createElement('p');
-    aboutText.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare urna blandit, fermentum ex non, efficitur arcu. Aliquam dui purus, ultricies ut efficitur at, volutpat ut felis. Nam interdum magna et arcu tincidunt maximus at sed arcu. Cras ac ligula quam. Quisque condimentum velit sed faucibus aliquam. Ut egestas vulputate malesuada. Nulla aliquam pharetra consequat. In viverra, sapien ut eleifend laoreet, quam lectus consectetur odio, sed interdum purus lectus et massa. Praesent sit amet efficitur ante, facilisis ornare dolor. Vivamus pulvinar vitae mauris sed aliquam. Aenean eros nulla, finibus eu porttitor vehicula, sodales quis lorem. Pellentesque eu vestibulum sapien. Ut scelerisque risus elit, sed cursus ligula semper non.';
-    aboutText.className = '';
+    aboutText.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare urna blandit, fermentum ex non, efficitur arcu.' + 
+    ' Aliquam dui purus, ultricies ut efficitur at, volutpat ut felis. Nam interdum magna et arcu tincidunt maximus at sed arcu.' +
+    ' Cras ac ligula quam. Quisque condimentum velit sed faucibus aliquam. Ut egestas vulputate malesuada. Nulla aliquam pharetra consequat.' +
+    ' In viverra, sapien ut eleifend laoreet, quam lectus consectetur odio, sed interdum purus lectus et massa.';
+    aboutText.className = 'bg-secondary p-2';
     aboutDiv.appendChild(aboutText);
+
+    const container = document.createElement('div'); //Här gör jag en div container för de två mindre textelementen. 
+    container.className = '';
+    aboutDiv.appendChild(container);
+
+    const text2 = document.createElement('p');
+    text2.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare urna blandit, fermentum ex non, efficitur arcu.';
+    text2.className = 'bg-secondary p-2 w-50 float-start border-end border-3 border-light';
+    container.appendChild(text2);
+
+    const text3 = document.createElement('p');
+    text3.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare urna blandit, fermentum ex non, efficitur arcu.';
+    text3.className = 'bg-secondary p-2 w-50 float-end border-start border-3 border-light';
+    container.appendChild(text3);
 
     const titleRight = document.createElement('h1'); // Titel för den högra delen av siten. 
     titleRight.innerHTML = 'Hobbies';
     titleRight.className = 'h1 mt-3';
     right.appendChild(titleRight);
+
+    const img1 = document.createElement('img');
+    img1.innerHTML = 'bild.jpg';
+    right.appendChild(img1);
+
 }
 
